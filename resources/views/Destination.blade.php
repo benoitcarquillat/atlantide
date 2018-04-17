146d323a958a7e91d58f141629bb3d38cf773c8b
Florian <florian.nalenne@gmail.com>
2018-04-17 16:01:48 +0200

    routes a 2 variables

---
+++ resources/views/Destination.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.app')
+
+@section('content')
+    <p>Je suis la page globale des destinations</p>
+@stop
---
+++ resources/views/evenementslist.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.app')
+
+@section('content')
+    <p>Je suis la page globale qui recense la liste des evenements</p>
+@stop
