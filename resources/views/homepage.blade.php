753dea067f5b52915898f4ccc09647a21c10fe96
Benoit <benoit.carquillat@gmail.com>
2018-04-20 14:49:34 +0200

    Ajout de pleins de trucs cool jor les bonnes typo toussa toussa

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,10 @@
 
 <div class="homepage">
         <div class="homepage__banner"></div>
+        <div class="container">
+			<p class="tag">Top destinations</p>
+			<h2 class="h2">Découvrez nos destinations coup de coeur</h2>
+        </div>
 </div>
 @endsection
 
--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -7,6 +7,7 @@
 
     <title>Atlantide</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800,900" rel="stylesheet">
 </head>
 <body>
 
