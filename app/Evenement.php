f3cafc183bc76d1c779001c3d4053699acb8c5b6
Benoit <benoit.carquillat@gmail.com>
2018-04-20 15:40:26 +0200

    ajout variable

--- app/Evenement.php
+++ app/Evenement.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evenement extends Model
 {
-    //
+    protected $fillable = [
+        'id','type', 'nom', 'description', 'img', 'dateDebut', 'dateFin', 'adresse', 'ville', 'codeP'
+    ];
 }
