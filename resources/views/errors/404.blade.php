89b9992dae6c39ad8401db39ec28bdd67f5d51e3
Florian <florian.nalenne@gmail.com>
2018-05-23 19:33:51 +0200

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,10 @@
 @extends('templates.app')
 
 @section('content')
-<p>MA 404</p>
-@stop
+<div class="page404">
+    <div class="message">
+        <h1>Il semblerait que vous vous soyez perdu...</h1>
+        <button type="button" class="btn"><a href="/">Revenir à l'accueil</a></button>
+    </div>
+</div>
+@endsection
